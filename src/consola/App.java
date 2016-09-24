98e68fff121f9598886150d3149f01e8c1cc2eee
Lupercar <hotolete@gmail.com>
2016-09-24 02:04:15 +0200

    Hago una consulta a la BBDD mediante un SELECT

--- src/consola/App.java
+++ src/consola/App.java
@@ -2,10 +2,13 @@ package consola;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
-
-
+import entidades.Cliente;
 import oracle.jdbc.OracleDriver;
 
 public class App {
@@ -15,14 +18,18 @@ public class App {
 //		2) Cargar el driver en la MVJ
 		DriverManager.registerDriver(new OracleDriver() ); 
 		
-//		forma alternativa de registrar el driver en la MVJ
-//		Class.forName("oracle.jdbc.OracleDriver").newInstance(); 
-		
-//		3)Crear la conexión a BBDD(sesion)
-		Connection conexion = DriverManager.getConnection(
+//		3) Crear conexion a BBDD
+		Connection conexion = DriverManager.getConnection( 
 				"jdbc:oracle:thin:@localhost:1521:pcarrier", 
 				"JA4BM3ON15", 
 				"usuario"); 
+		
+		List<Cliente> clientes = new ArrayList<>(); 
+		
+//		4) Hacer operaciones con la BBDD
+		Statement comando = conexion.createStatement(); //creamos un comando nuevo 
+		String sql = "SELECT id, nombre FROM cliente"; 
+		ResultSet resultados = comando.executeQuery(sql); 
 		
 //		Cierrao conexión a BBDD
 		conexion.close(); 
